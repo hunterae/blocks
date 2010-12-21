695f51636aacab03b77e8194af6fcaa1b76a5843
hunterae <hunterae@gmail.com>
2010-12-21 17:03:40 -0500

    Fixed before and after methods to return nil (since content is rendered directly into the template) and to only render before and after filters once if in the global scope

--- lib/blocks/builder.rb
+++ lib/blocks/builder.rb
@@ -4,58 +4,8 @@ module Blocks
     attr_accessor :view, :blocks, :block_positions, :anonymous_block_number,
                   :start_rendering_blocks, :block_groups, :shared_options
     
-    # If a method is missing, we'll assume the user is starting a new block group by that missing method name
-    def method_missing(m, options={}, &block) 
-      # If the specified block group has already been defined, it is simply returned here for iteration.
-      #  It will consist of all the blocks used in this block group that have yet to be rendered,
-      #   as the call for their use occurred before the template was rendered (where their definitions likely occurred)
-      return self.block_groups[m] unless self.block_groups[m].nil?
-      
-      # Allows for nested block groups, store the current block positions array and start a new one
-      original_block_positions = self.block_positions
-      self.block_positions = []
-      self.block_groups[m] = self.block_positions
-     
-      # Capture the contents of the block group (this will only capture block definitions and block uses)
-      view.capture(shared_options.merge(options), &block) if block_given?
-      
-      # restore the original block positions array
-      self.block_positions = original_block_positions
-      nil
-    end
-    
-    def before(name, options={}, &block)
-      name = "before_#{name.to_s}".to_sym
-      
-      block_container = Blocks::Container.new
-      block_container.name = name
-      block_container.options = options
-      block_container.block = block
-      
-      if blocks[name].nil?
-        blocks[name] = [block_container]
-      else
-        blocks[name] << block_container
-      end
-
-      nil
-    end
-    
-    def after(name, options={}, &block)
-      name = "after_#{name.to_s}".to_sym
-      
-      block_container = Blocks::Container.new
-      block_container.name = name
-      block_container.options = options
-      block_container.block = block
-      
-      if blocks[name].nil?
-        blocks[name] = [block_container]
-      else
-        blocks[name] << block_container
-      end
-
-      nil
+    def defined?(name)
+      !blocks[name.to_sym].nil?
     end
     
     def define(name, options={}, &block)
@@ -67,6 +17,11 @@ module Blocks
       blocks[name.to_sym] = block_container if blocks[name.to_sym].nil?
       
       nil
+    end
+    
+    def replace(name, options, &block)
+      blocks[name.to_sym] = nil
+      define(name, options, block)
     end
   
     def use(*args, &block)      
@@ -103,6 +58,60 @@ module Blocks
       self.start_rendering_blocks = true
 
       view.render shared_options[:template], shared_options
+    end
+    
+    def before(name, options={}, &block)
+      name = "before_#{name.to_s}".to_sym
+      
+      block_container = Blocks::Container.new
+      block_container.name = name
+      block_container.options = options
+      block_container.block = block
+      
+      if blocks[name].nil?
+        blocks[name] = [block_container]
+      else
+        blocks[name] << block_container
+      end
+
+      nil
+    end
+    
+    def after(name, options={}, &block)
+      name = "after_#{name.to_s}".to_sym
+      
+      block_container = Blocks::Container.new
+      block_container.name = name
+      block_container.options = options
+      block_container.block = block
+      
+      if blocks[name].nil?
+        blocks[name] = [block_container]
+      else
+        blocks[name] << block_container
+      end
+
+      nil
+    end
+    
+    # If a method is missing, we'll assume the user is starting a new block group by that missing method name
+    def method_missing(m, options={}, &block) 
+      # If the specified block group has already been defined, it is simply returned here for iteration.
+      #  It will consist of all the blocks used in this block group that have yet to be rendered,
+      #   as the call for their use occurred before the template was rendered (where their definitions likely occurred)
+      return self.block_groups[m] unless self.block_groups[m].nil?
+      
+      # Allows for nested block groups, store the current block positions array and start a new one
+      original_block_positions = self.block_positions
+      self.block_positions = []
+      self.block_groups[m] = self.block_positions
+     
+      # Capture the contents of the block group (this will only capture block definitions and block uses)
+      view.capture(shared_options.merge(options), &block) if block_given?
+      
+      # restore the original block positions array
+      self.block_positions = original_block_positions
+      nil
     end
     
     protected
@@ -164,11 +173,13 @@ module Blocks
         end
       end
       
-      unless view.blocks.blocks[name].nil?
+      unless view.blocks.blocks[name].nil? || view.blocks.blocks == blocks
         view.blocks.blocks[name].each do |block_container|
           view.concat(view.capture shared_options.merge(block_container.options).merge(options), &block_container.block)
         end
       end
+      
+      nil
     end
     
     def render_after_blocks(name, options={})
@@ -180,11 +191,13 @@ module Blocks
         end
       end
       
-      unless view.blocks.blocks[name].nil?
+      unless view.blocks.blocks[name].nil? || view.blocks.blocks == blocks
         view.blocks.blocks[name].each do |block_container|
           view.concat(view.capture shared_options.merge(block_container.options).merge(options), &block_container.block)
         end
       end
+      
+      nil
     end
   end
 end
