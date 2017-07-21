cf76f0bf47c12bb38962e4b10c3ccc9a6e48bd91
Andrew Hunter <hunterae@gmail.com>
2017-07-21 11:23:21 -0600

    Fixed rspec configuration to work on all Travis configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,6 @@ RSpec.configure do |config|
   end
 
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.filter_run_when_matching :focus
+  config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 end
