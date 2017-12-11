64a57cf5500a7d55cd56b4dfdf2116ada7656f35
Andrew Hunter <hunterae@gmail.com>
2017-12-11 11:08:32 -0700

    Removed use of deep_dup method when rendering a partial. #deep_dup was causing activerecord relations that were paginated with will_paginate in Rails 4 to lose their pagination attributes

--- lib/blocks/renderers/partial_renderer.rb
+++ lib/blocks/renderers/partial_renderer.rb
@@ -2,7 +2,7 @@ module Blocks
   class PartialRenderer < AbstractRenderer
     def render(partial, options={}, &block)
       if !options.is_a?(Blocks::RuntimeContext)
-        options = RuntimeContext.new(builder, options).to_hash.with_indifferent_access.deep_dup
+        options = RuntimeContext.new(builder, options).to_hash.with_indifferent_access
       end
       overrides_and_provided_content = capture(builder, options, &block) if block_given?
       locals = options.merge(
